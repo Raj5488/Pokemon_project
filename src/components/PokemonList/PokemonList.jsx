81b11fad1ee537716e686058735ec2f733874993
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-08-16 12:34:59 +0530

    Pokemon_Project

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -15,6 +15,16 @@ function PokemonList(){
             const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
             const pokemonData = await axios.all(pokemonResultPromise)
             console.log(await pokemonData);
+            const res = pokemonData.map((pokeData) => {
+                const pokemon = pokeData.data;
+                return {
+                    name: pokemon.name, 
+                    image: (pokemon.sprites.other) ? pokemon.sprites.dream_world.front_default : pokemon.sprites.front_shiny,
+                    types: pokemon.types
+                    }
+            })
+            console.log(res);
+            setPokemonList(res);
             setIsLoading(false)
         }
 
