8b92d800f7971c38c98d814bb48e474787c8ac63
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-08-16 11:42:43 +0530

    Pokemon_InArray

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from 'axios'
-
+import "./pokemonList.css"
 
 function PokemonList(){
 
+    const [pokemonList, setPokemonList] = useState([]);
+    const [isLoading, setIsLoading] =useState(true);
+
+
         async function downloadPokemon(){
             const response = axios.get("https://pokeapi.co/api/v2/pokemon")
-            console.log(response);
+            const pokemonResults = (await response).data.results
+            const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
+            const pokemonData = await axios.all(pokemonResultPromise)
+            console.log(await pokemonData);
+            setIsLoading(false)
         }
-
-
 
     useEffect( () =>{
         downloadPokemon()
     })
-
     
     return(
         <div className="pokemon-list-wrapper">
-                pokemonList
+                <div>pokemonList </div>
+                {(isLoading) ? 'Loading...' : 'Data_downloaded'}
         </div>
     )
 }
